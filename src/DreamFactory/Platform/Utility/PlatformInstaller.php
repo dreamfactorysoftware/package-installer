b5b088ce96e283e3e26b61341b70989193f5c7a4
lucifurious <jerryablan@gmail.com>
2013-03-21 13:31:59 -0400

    tweakage

--- src/DreamFactory/Platform/Utility/PlatformInstaller.php
+++ src/DreamFactory/Platform/Utility/PlatformInstaller.php
@@ -1,6 +1,8 @@
 <?php
 namespace DreamFactory\Platform\Utility;
 
+use Composer\Composer;
+use Composer\IO\IOInterface;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
@@ -22,45 +24,115 @@ class PlatformInstaller extends LibraryInstaller
 	/**
 	 * @var string
 	 */
-	const BASE_INSTALL_PATH = '/dsp-share';
+	const BASE_INSTALL_PATH = '/shared';
 	/**
 	 * @var string
 	 */
 	const DEFAULT_INSTALL_NAMESPACE = 'app';
+	/**
+	 * @var string
+	 */
+	const FABRIC_MARKER = '/var/www/.fabric_hosted';
+	/**
+	 * @var string
+	 */
+	const FABRIC_INSTALL_PATH = '/var/www/dsp-share';
+
+	//*************************************************************************
+	//* Members
+	//*************************************************************************
+
+	/**
+	 * @var bool
+	 */
+	protected $_fabricHosted = false;
 
 	//*************************************************************************
 	//* Methods
 	//*************************************************************************
 
 	/**
+	 * @param IOInterface $io
+	 * @param Composer    $composer
+	 * @param string      $type
+	 */
+	public function __construct( IOInterface $io, Composer $composer, $type = 'library' )
+	{
+		parent::__construct( $io, $composer, $type );
+
+		$this->_fabricHosted = file_exists( static::FABRIC_MARKER );
+	}
+
+	/**
 	 * {@inheritDoc}
 	 */
 	public function getInstallPath( PackageInterface $package )
 	{
-		$_installPath = dirname( dirname( $this->vendorDir ) ) . static::BASE_INSTALL_PATH;
 		$_parts = explode( '/', $package->getPrettyName(), 2 );
 
 		if ( static::PACKAGE_PREFIX != ( $_prefix = @current( $_parts ) ) )
 		{
 			throw new \InvalidArgumentException(
-				'This package is not a DreamFactory package and cannot be installed.' . PHP_EOL .
+				'This package is not a DreamFactory package and cannot be installed by this installer.' . PHP_EOL .
 					'  * Name: ' . $package->getPrettyName() . PHP_EOL .
 					'  * Parts: ' . print_r( $_parts, true ) . PHP_EOL
 			);
 		}
 
+		if ( $this->_fabricHosted )
+		{
+			return $this->_buildInstallPath( static::FABRIC_INSTALL_PATH, $_prefix, $_parts );
+		}
+
+		//	Effectively /docRoot/shared/[vendor]/[namespace]/[package]
+		return $this->_buildInstallPath(
+			realpath( dirname( $this->vendorDir ) . static::BASE_INSTALL_PATH ),
+			$_prefix,
+			$_parts );
+	}
+
+	/**
+	 * Build the install path
+	 *
+	 * @param string $baseInstallPath
+	 * @param string $prefix
+	 * @param array  $parts
+	 * @param bool   $createIfMissing
+	 *
+	 * @throws \InvalidArgumentException
+	 * @return string
+	 */
+	protected function _buildInstallPath( $baseInstallPath, $prefix, $parts, $createIfMissing = true )
+	{
 		/**
 		 *    Package like dreamfactory/app-xyz or dreamfactory/lib-abc will
 		 *    go into ./apps/app-xyz and ./lib/lib-abc respectively)
 		 */
 
-		$_fullPath = realpath( $_installPath ) . '/' . $_prefix . '/' .
-			( @current( @explode( '-', end( $_parts ), 2 ) ) ? :
-				static::DEFAULT_INSTALL_NAMESPACE ) . '/' . $_parts[1];
+		//	i.e. /var/www/dsp-share/dreamfactory/
+		$_fullPath = $baseInstallPath . '/' . $prefix . '/';
 
-		if ( !is_dir( $_fullPath ) )
+		//	Split package type off of front (app-*, lib-*, web-*, etc.)
+		$_subparts = explode( '-', $parts[1], 2 );
+
+		if ( empty( $_subparts ) )
 		{
-			@mkdir( $_fullPath, 0777, true );
+			$_namespace = static::DEFAULT_INSTALL_NAMESPACE;
+		}
+		else
+		{
+			$_namespace = $_subparts[0];
+		}
+
+		//	/var/www/dsp-share/dreamfactory/[namespace]/[package]
+		$_fullPath .= $_namespace . '/' . $parts[1];
+
+		if ( $createIfMissing && !is_dir( $_fullPath ) )
+		{
+			if ( false === @mkdir( $_fullPath, 0777, true ) )
+			{
+				throw new \InvalidArgumentException( 'Unable to create installation path "' . $_fullPath . '"' );
+			}
 		}
 
 		return $_fullPath;
